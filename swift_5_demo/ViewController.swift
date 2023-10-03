0580015d1768c86e1dbcf6f552ffffc2e94d90e6
hai.wen <hai.wen@bytedance.com>
2023-10-03 16:22:21 +0800

    init repo with xcode

---
+++ swift_5_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_5_demo
+//
+//  Created by wenhai on 3/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swift_5_demoUITests/swift_5_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swift_5_demoUITestsLaunchTests.swift
+//  swift_5_demoUITests
+//
+//  Created by wenhai on 3/10/23.
+//
+
+import XCTest
+
+final class swift_5_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
